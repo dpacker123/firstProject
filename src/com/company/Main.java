23024c4621ee6f1a89c83cdfa115b5ae7994571d
dpacker123 <89939888+dpacker123@users.noreply.github.com>
2021-09-17 10:55:23 -0400

    working commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-	    var filename = "Products.txt";
+        var filename = "Products.txt";
         var products = Paths.get(filename);
         var productName = new ArrayList<String>();
         var productPrice = new ArrayList<Float>();
@@ -20,24 +21,30 @@ public class Main {
             productPrice.add(Float.valueOf(splitline[1]));
             productCount.add(Integer.valueOf(splitline[2]));
         }
+
         //System.out.println("Product name: " +productName+ "  Product price: " +productPrice+ "  Product count: " +productCount);
         var reader = new Scanner(System.in);
         var userResponse = "";
-        while (!userResponse.equals("end")) {
-            System.out.print("Hello, what would you like to look up? At ny time you can end the program by typing 'end'.");
+        System.out.print("Hello, what would you like to look up? At any time you can end the program by typing 'end'.");
 
+        while (!userResponse.equals("end")) {
             userResponse = reader.nextLine();
+
             if (userResponse.equals("end")) {
                 System.out.print("Goodbye");
                 break;
             }
 
             if (productName.contains(userResponse)) {
-                System.out.println("Yes, we do have: " + userResponse);
+                var productSearch = productName.indexOf(userResponse);
+                var price =productPrice.get(productSearch);
+                var count = productCount.get(productSearch);
+                System.out.println("Yes, we have " +count+ " " +userResponse+ " priced at " +price+ " dollars per "+userResponse);
             }
             else {
                 System.out.print("I'm sorry but we do not have that, please check for a different product");
             }
         }
     }
+
 }
